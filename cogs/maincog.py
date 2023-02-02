ce8f5c83be88c2b6ca4a7efec539fc9cfa73319f
TimelessOn15Hz <timeless.off.tv@gmail.com>
2023-02-03 00:47:59 +0200

    Better /add command, general improvements

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -3,19 +3,21 @@ import pymongo
 import os
 import random
 
-from discord import app_commands
+from discord import app_commands, ui
 from discord.ext import commands
 
 from dotenv import load_dotenv
+from datetime import datetime
+now = datetime.now()
 
 
 #DOTENV
 load_dotenv()
-PASS = os.getenv('MONGODB_PASSWORD')
+MONGODB = os.getenv('MONGODB_PASSWORD')
 
 
-client = pymongo.MongoClient(f"mongodb+srv://TimelessOn15Hz:{PASS}@cluster0.03k6m8r.mongodb.net/?retryWrites=true&w=majority")
-id_db = client.registered_ids
+mdbclient = pymongo.MongoClient(f"{MONGODB}")
+registered_ids = mdbclient.registered_ids
 
 
 #COG CLASS
@@ -29,31 +31,43 @@ class Test(commands.Cog):
     async def ping(self, interaction: discord.Interaction) -> None:
         await interaction.response.send_message('Ping: {0}ms'.format(round(self.bot.latency, 3)), ephemeral=False)
 
+    
+    @app_commands.command(name="myhomework", description="Lists all of your homework!")
+    #Cycles through _idlist with buttons
+    #Has a (Mark Complete) button
+    #Has a (Delete) button
+    async def myhomework(self, interaction: discord.Interaction) -> None:
+        await interaction.response.defer(ephemeral=True)
+        author = interaction.user.id
+        key = {'author': author}
+        _idlist = "\r\n".join(f"> {id['id']}" for id in registered_ids.homework.find(key))
+        print(_idlist)
+        await interaction.followup.send(f"Your IDs:\n\n{_idlist}")
 
-    @app_commands.command(name="register", description="Registers an ID to the database")
-    async def register(self, interaction: discord.Interaction, title: str) -> None:       
-        id_db.ids.insert_one(
+
+    @app_commands.command(name="add", description="Adds homework to your list!")
+    async def add(self, interaction: discord.Interaction) -> None:
+        await interaction.response.send_modal(addHW())
+
+class addHW(ui.Modal, title="Add your homework!"):
+
+    hwtitle = ui.TextInput(label="Title", style=discord.TextStyle.short, placeholder="Add a title!", required=True)
+    subject = ui.TextInput(label="Subject", style=discord.TextStyle.short, placeholder="What subject?", required=True)
+    description = ui.TextInput(label="Description", style=discord.TextStyle.paragraph, placeholder="Give a description (optional)", required=False)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        result = registered_ids.homework.insert_one(
             {
-                "title": title,
-                "id": id,
+                "title": str(self.hwtitle),
+                "subject": str(self.subject),
+                "description": str(self.description),
+                "complete": bool(False),
+                "date": str(now.strftime("%d/%m/%Y %H:%M")),
                 "author": interaction.user.id
             }
         )
-        await interaction.response.send_message("ID was registered!")
-
-    
-    @app_commands.command(name="ping", description="Shows the latency of the bot.")
-    async def ping(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message('Ping: {0}ms'.format(round(self.bot.latency, 3)), ephemeral=False)
-
-
-    @app_commands.command(name="myid", description="Retrieves author's registered IDs")
-    async def myid(self, interaction: discord.Interaction) -> None:
-        await interaction.response.defer()
-        author = interaction.user.id
-        key = {'author': author}
-        _idlist = "\r\n".join(f"> {id['id']}" for id in id_db.ids.find(key))
-        await interaction.followup.send(f"Your IDs:\n\n{_idlist}")
+        id = result.inserted_id
+        await interaction.response.send_message(f"Homework added successfully!\n> Unique ID: {id}", ephemeral=True)
 
 
 async def setup(bot):
