2b4711dd59a28a02740c5a67a6f5fcc7d2e4664a
TeeTwoNot <85189523+TeeTwoNot@users.noreply.github.com>
2023-12-25 23:16:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,136 +1,220 @@
+"""
+main.py
+~~~~~~~~~~~~~~~~~~~
+Main file.
+"""
+
 #IMPORTS
 import os
-import discord
 import datetime
 import asyncio
 import sys
+import platform
+import logging
+import discord
 
-
-#FROM DISCORD
-from discord import app_commands
-from discord.ext import commands, tasks
-
-
-#FROM OTHERS
+from typing import Final
+from discord.ext import commands
+from app import app
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+from colorama import init as colorama_init
+from colorama import Fore, Style
 from dotenv import load_dotenv
-from datetime import datetime
 
+load_dotenv()
+
+colorama_init(autoreset=True)
 
 #INTENTS
-intents = discord.Intents.all()
+intents = discord.Intents.default()
+intents.presences = True
 intents.members = True
+intents.message_content = True
 
 
 #DON'T GENERATE __PYCACHE__
 sys.dont_write_bytecode = True
 
+#LOGGING
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '{1}[%(asctime)s]{0} {2}[%(levelname)s]{0} {3}%(name)s{0} - %(message)s'.format(
+        Style.RESET_ALL,
+        Fore.LIGHTBLACK_EX,
+        Fore.BLUE,
+        Fore.MAGENTA,
+        ),
+    "%d-%m-%Y %H:%M:%S"
+    )
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
+logging.getLogger('hypercorn.access').setLevel(logging.ERROR)
 
 #DOTENV
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-PASS = os.getenv('MONGODB_PASSWORD')
+TOKEN : Final[str] = os.environ['DISCORD_TOKEN']
 
-
-#HOMEWORK TRACKER GUILD ID (FOR SYNCING)
-MY_GUILD = discord.Object(id=1020373328164294726)
-
+#HYPERCORN CONFIG
+config = Config()
+config.bind = [f"0.0.0.0:80"]
 
 #BOT CLASS
-class Bot(commands.Bot):
+class Bot(commands.AutoShardedBot):
     def __init__(self):
         super().__init__(command_prefix="/", help_command=None, intents=intents)
+        self.discord_version = discord.__version__
+        self.platform = platform.python_version()
 
     #ON_READY
     async def on_ready(self):
-        timestamp = datetime.now()
+        c = (
+            Style.RESET_ALL,
+            Fore.LIGHTGREEN_EX,
+            Fore.LIGHTCYAN_EX,
+            Fore.MAGENTA,
+            Fore.LIGHTBLACK_EX
+            )
+        timestamp = datetime.datetime.now()
+        amount = len(bot.guilds)
+        shards = bot.shard_count
+        print(f'{c[4]}----------------------')
+        print(f'Homework Tracker is online and in {amount} servers!')
+        print(f'{c[2]}Shards:{c[0]} {shards}')
+        print(f'{c[2]}Ping:{c[0]} {round(bot.latency, 3)}ms')
+        print(f'{c[2]}API:{c[0]} {self.discord_version}')
+        print(f'{c[2]}Python:{c[0]} {self.platform}')
+        print(timestamp.strftime(f"{c[3]}T: %d/%m/%Y %H:%M:%S\n{c[4]}----------------------"))
 
-        print("-------------------")
-        print('Homework Tracker is ONLINE!')
-        print(timestamp.strftime("T: %d/%m/%Y %H:%M:%S"))
-        print('Ping: {0}ms'.format(round(bot.latency, 3)))
-        print("-------------------")
-
-        #BOT STATUS LOOP
+        #BOT LOOPS
         bot.loop.create_task(status_task())
-    
-        #START MEMBER COUNT LOOP
-        membercount.start()
 
     #LOAD COGS & SYNC SLASH COMMANDS
     async def setup_hook(self):
+        c = (
+            Style.RESET_ALL,
+            Fore.LIGHTBLACK_EX,
+            Fore.YELLOW,
+            Fore.RED
+            )
         for filename in os.listdir('./cogs'):
             if filename.startswith("__pycache__"):
                 continue
-            if filename.endswith('.py'):
-                await bot.load_extension(f'cogs.{filename[:-3]}')
-                print(f"-------------------\nLoaded {filename}")
+            if filename.startswith("indev"):
+                continue
+            if filename.endswith(".py"):
+                await bot.load_extension(f"cogs.{filename[:-3]}")
+                print(f"{c[1]}----------------------{c[0]}\nLoaded {c[2]}{filename}")
             else:
-                print(f"-------------------\nError loading {filename}")
+                print(f"{c[1]}----------------------\n{c[3]}Error loading {c[2]}{filename}")
 
         await self.tree.sync()
-        print("-------------------")
+        print(f"{c[1]}----------------------")
         print("Synced Slash Commands")
-        print("-------------------")
-
-
-#MEMBER COUNT CHANNEL LOOP
-@tasks.loop(minutes=30)
-async def membercount():
-    amount = 0
-    guild = bot.get_guild(1020373328164294726)
-    channel = bot.get_channel(1020381747969532075)
-    if amount != guild.member_count:
-        await channel.edit(name=f"Members: {guild.member_count}")
-
-
-@membercount.before_loop
-async def before_loop_occur():
-    await bot.wait_until_ready()
+        print(f"{c[1]}----------------------")
 
 
 #DEFINE BOT
 bot = Bot()
 
-
 #LOAD COG COMMAND
 @bot.command(hidden=True)
 @commands.is_owner()
 async def load(ctx, extension):
-    await bot.load_extension(f'cogs.{extension}')
-    await ctx.channel.send(f"Loaded **{extension}**", delete_after=1)
-    await ctx.channel.purge(amount=1)
+    await bot.load_extension(f"cogs.{extension}")
+    await ctx.channel.send(f"Loaded **{extension}.py** successfully!")
 
 
 #UNLOAD COG COMMAND
 @bot.command(hidden=True)
 @commands.is_owner()
 async def unload(ctx, extension):
-    await bot.unload_extension(f'cogs.{extension}')
-    await ctx.channel.send(f"Unloaded **{extension}**", delete_after=1)
-    await ctx.channel.purge(amount=1)
+    await bot.unload_extension(f"cogs.{extension}")
+    await ctx.channel.send(f"Unloaded **{extension}.py** successfully!")
 
 
 #RELOAD COG COMMAND
 @bot.command(hidden=True)
 @commands.is_owner()
 async def reload(ctx, extension):
-    await bot.unload_extension(f'cogs.{extension}')
-    await bot.load_extension(f'cogs.{extension}')
-    await ctx.channel.send(content=f"Reloaded **{extension}** succesfully", delete_after=1)
-    await ctx.channel.purge(amount=1)
+    await bot.unload_extension(f"cogs.{extension}")
+    await bot.load_extension(f"cogs.{extension}")
+    await ctx.channel.send(content=f"Reloaded **{extension}.py** successfully!")
+
+
+#LOADINDEV COG COMMAND
+@bot.command(hidden=True)
+@commands.is_owner()
+async def loadindev(ctx):
+    for filename in os.listdir('./cogs/indev'):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.indev.{filename[:-3]}")
+            await ctx.channel.send(f"Loaded **{filename}** successfully!")
+            print(f"----------------------\nLoaded {filename}")
+            await asyncio.sleep(1)
+
+
+#UNLOADINDEV COG COMMAND
+@bot.command(hidden=True)
+@commands.is_owner()
+async def unloadindev(ctx):
+    for filename in os.listdir('./cogs/indev'):
+        if filename.endswith(".py"):
+            await bot.unload_extension(f"cogs.indev.{filename[:-3]}")
+            await ctx.channel.send(f"Unloaded **{filename}** successfully!")
+            print(f"----------------------\nLoaded {filename}")
+            await asyncio.sleep(1)
+
+
+#SHUTDOWN COMMAND
+@bot.command(hidden=True)
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.channel.send("Shutting down...")
+    await bot.close()
 
 
 #BOT STATUS
 @bot.event
 async def status_task():
+    amount = len(bot.guilds)
     while True:
-        await bot.change_presence(activity=discord.Game(name="Now in Alpha!"))
+        await bot.change_presence(
+            activity = discord.Game(name="Prefix: /")
+            )
         await asyncio.sleep(120)
-        await bot.change_presence(activity=discord.Game(name="beep boop"))
+        await bot.change_presence(
+            activity=discord.Game(name="beep boop")
+            )
         await asyncio.sleep(120)
-        await bot.change_presence(activity=discord.Game(name="Supports Slash Commands!"))
+        await bot.change_presence(
+            activity=discord.Game(name="Now supports Slash Commands!")
+            )
+        await asyncio.sleep(120)
+        await bot.change_presence(
+            activity=discord.Game(name=f"in {amount} servers!")
+            )
+        await asyncio.sleep(120)
+        await bot.change_presence(
+            activity=discord.Game(name="boop beep boop?")
+            )
+        await asyncio.sleep(120)
+        await bot.change_presence(
+            activity=discord.Activity(
+                type=discord.ActivityType.watching,
+                name="TeeTwoNot while he sleeps"
+                )
+            )
         await asyncio.sleep(120)
 
+async def main():
+    async with bot:
+        bot.loop.create_task(serve(app, config))
+        await bot.start(TOKEN)
 
-
-bot.run(TOKEN)
+if __name__ == '__main__':
+    asyncio.run(main())
